c365569f1da3f526515a0ecaa2fe78ce17aa6248
synjinshanley <54914702+synjinshanley@users.noreply.github.com>
2020-03-01 09:56:51 -0500

    Add files via upload

---
+++ wos-project/public/js/HandleOrder.js
@@ -0,0 +1,74 @@
+/**
+ *  @author synjin, shanley (synjinshanley@gmail.com)
+ *  @version 0.0.1
+ *  @summary WOS project || created: 2/2/20
+ *  @todo
+ */
+ 
+ export default class EventHandler {
+
+constructor(){
+    this.handleOrder();
+}
+
+handleOrder() {
+    let removeMe;
+    document.getElementById('enter').addEventListener('click', removeMe = () => {
+        this.setDateYear(document.getElementById('year').value);
+        this.setDateMonth(document.getElementById('month').value);
+        this.setDateDay(document.getElementById('day').value);
+        this.setBuilding(document.getElementById('building').value);
+        this.setRoom(document.getElementById('room').value);
+        this.setOrder(document.getElementById('discrip').value);
+        this.setList();
+        this.setWorkOrder();
+        this.setSave
+    }
+
+    setDateYear(year){
+        this.dateYear = year;
+    }
+
+    setDateMonth(month){
+        this.dateMonth = month;
+    }
+
+    setDateDay(day){
+        this.dateDay = day;
+    }
+
+    setBuilding(building){
+        this.building = building;
+    }
+
+    setRoom(room){
+        this.room = room;
+    }
+
+    setOrder(discrip){
+        this.order = discrip;
+    }
+
+    setList(this.dateYear, this.dateMonth, this.dateDay, this.building, this.room, this.order){
+        let this.list {
+            year: `${this.dateYear}`,
+            month: `${this.dathMonth}`,
+            day: `${this.dateDay}`,
+            building: `${this.building}`,
+            room: `${this.room}`,
+            order: `${this.order}`
+        }
+    }
+
+    setWorkOrder(this.list){
+        let this.workOrder = [];
+        this.workOrder.push(this.list);
+		document.getElementById('order').innerHTML = `<p>${this.workOrder}</p>`;
+    }
+
+    setSave(){
+        save = IO.unlink(`master.txt`)
+        save = IO.appendfile(`master.txt, main.js`)
+    }
+
+}
--- wos-project/public/js/main.js
+++ wos-project/public/js/main.js
@@ -4,68 +4,17 @@
  *  @summary WOS project || created: 2/2/20
  *  @todo
  */
+ 
+ import EventHandler from './EventHandler.js';
+ 
+export default class Main {
 
-constructor(){
-    this.handleOrder();
-}
-
-handleOrder() {
-    let removeMe;
-    document.getElementById('enter').addEventListener('click', removeMe = () => {
-        this.setDateYear(document.getElementById('year').value);
-        this.setDateMonth(document.getElementById('month').value);
-        this.setDateDay(document.getElementById('day').value);
-        this.setBuilding(document.getElementById('building').value);
-        this.setRoom(document.getElementById('room').value);
-        this.setOrder(document.getElementById('discrip').value);
-        this.setList();
-        this.setWorkOrder();
-        this.setSave
-    }
-
-    setDateYear(year){
-        this.dateYear = year;
-    }
-
-    setDateMonth(month){
-        this.dateMonth = month;
-    }
-
-    setDateDay(day){
-        this.dateDay = day;
-    }
-
-    setBuilding(building){
-        this.building = building;
-    }
-
-    setRoom(room){
-        this.room = room;
-    }
-
-    setOrder(order){
-        this.order = order;
-    }
-
-    setList(this.dateYear, this.dateMonth, this.dateDay, this.building, this.room, this.order){
-        let this.list {
-            year: `${this.dateYear}`,
-            month: `${this.dathMonth}`,
-            day: `${this.dateDay}`,
-            building: `${this.building}`,
-            room: `${this.room}`,
-            order: `${this.order}`
-        }
-    }
-
-    setWorkOrder(this.list){
-        let this.workOrder = [];
-        this.workOrder.push(this.list);
-    }
-
-    setSave(){
-        save = IO.unlink(`master.txt`)
-        save = IO.appendfile(`master.txt, main.js`)
-    }
+	constructor(){
+		new HandleOrder();
+	}
 
 }
+
+window.addEventListener('load', () => {
+    new Main();
+});
